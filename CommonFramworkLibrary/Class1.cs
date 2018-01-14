73e7059c97dea3b415cd712e9af3278edbfe63c8
mfaci2000 <mfaci2000@yahoo.com>
2018-01-14 13:32:30 -0600

    Add projects to solution to use use page object model.

---
+++ CommonFramworkLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CommonFramworkLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c0750e3-0bcf-469c-b3bd-bb357564c78b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
